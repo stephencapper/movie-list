54b4f9c943f9322fab42b334825d66bf11f0e6c7
Stephen Capper <stephencapper@gmail.com>
2024-02-08 18:52:10 -0800

    additional files for imitial work on watched functionality

---
+++ client/src/components/ToggleWatchedDisplay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ToggleWatchedDisplay = (props) {
+  //some code
+  return (
+    <div>Toggle Watched Display</div>
+  )
+};
+
+export default ToggleWatchedDisplay;
+
+
+
+
---
+++ client/src/components/WatchedMovie.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const WatchedMovie = (props) => {
+  const watchedClickHandler = (event) => {
+    let movieWatched = event.target.value === 'Watched' ? 'watched' : 'toWatch';
+    let movieTitle = props.movie.title;
+    let moviesData = props.moviesData.slice(0);
+    for (let movie of moviesData) {
+      if (movie.title === movieTitle) {
+        movie.watched = movieWatched;
+        break;
+      }
+    }
+    props.setMoviesData(moviesData);
+    if (props.watchedDisplay === 'AllMovies') {
+      props.setCurrentMovies(moviesData);
+    } else {
+      let currentMovies = props.currentMovies.slice(0);
+      for (let i = 0; i < currentMovies.length; i++) {
+        if ((currentMovies[i]).title === movieTitle) {
+          // for (let j = i; j < currentMovies.length - 1; j++) {
+          //   currentMovies[j] = currentMovies[j + 1];
+          // }
+          currentMovies.pop();
+          break;
+        }
+      }
+      props.setCurrentMovies(currentMovies);
+    }
+  };
+  return (
+    <>
+      {props.movie.watched === 'watched' ? <button onClick={watchedClickHandler}>Watched</button> : null}
+      {props.movie.watched === 'toWatch' ? <button onClick={watchedClickHandler}>To watch</button> : null}
+    </>
+  );
+};
+
+export default WatchedMovie;
+
+    /*{props.movie.watched === 'watched' ?
+      <button onClick={watchedClickHandler}>Watched<button/>
+    :
+      <button onClick={watchedClickHandler}>To watch<button/>
+    }*/
